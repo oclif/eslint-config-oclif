493da9218a4a9ae70c75627aed5cfae8e2367d6b
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-09-23 12:17:45 -0400

    chore: re-add unicorn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-module.exports = {
+module.exports = { // eslint-disable-line unicorn/prefer-module
   extends: [
     'eslint:recommended',
-    'plugin:node/recommended',
     'xo-space',
+    'plugin:node/recommended',
+    'plugin:unicorn/recommended',
   ],
   plugins: [
     'node',
     'mocha',
+    'unicorn',
   ],
   rules: {
     'capitalized-comments': 0,
@@ -18,6 +20,7 @@ module.exports = {
     indent: ['error', 2, {SwitchCase: 0, MemberExpression: 0}],
     quotes: ['error', 'single', {avoidEscape: true}],
     semi: ['error', 'never'],
+    'unicorn/prevent-abbreviations': 'off',
   },
   globals: {
     describe: true,
